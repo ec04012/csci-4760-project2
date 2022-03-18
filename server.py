10b59e8b58888f1bb5df46be041d4981b2297901
ec04012 <ec04012@uga.edu>
2022-03-17 22:51:43 -0400

    Added animation to server.py

--- server.py
+++ server.py
@@ -4,11 +4,15 @@ from socket import *
 import sys # In order to terminate the program
 import os
 import atexit
+import itertools
+import threading
+import time
 
 # Source
 # https://www.codementor.io/@joaojonesventura/building-a-basic-http-server-from-scratch-in-python-1cedkg0842
 # https://www.geeksforgeeks.org/python-os-path-join-method/
 # https://docs.python.org/3/library/atexit.html
+# https://stackoverflow.com/questions/22029562/python-how-to-make-simple-animated-loading-while-process-is-running
 
 # graceful exit in case of crash or other disruption
 def graceful_exit():
@@ -16,6 +20,21 @@ def graceful_exit():
     print("Closing Server")
     sys.exit()#Terminate the program
 atexit.register(graceful_exit)
+
+animating = False
+# animation function
+def animate():
+    for c in itertools.cycle(['|', '/', '-', '\\']):
+        if animating:
+                print('\r     Waiting ' + c, end="")
+                time.sleep(0.25)
+def stopAnimating():
+        global animating
+        animating=False
+        print('\r                               \r', end="")
+# Create animation thread
+t = threading.Thread(target=animate, daemon=True)
+t.start()
 
 # Define socket host and port
 SERVER_HOST = '0.0.0.0'
@@ -30,11 +49,14 @@ ServerName = "HTTP Server"
 print(ServerName + " is now ONLINE")
 print('Listening on port %s ...' % SERVER_PORT)
 print("")
+
 while True:
     #Establish the connection
     print('Listening for requests ...')
+    animating = True
     # Wait for client connections
     connectionSocket, addr = serverSocket.accept()
+    stopAnimating()
     print("Connected to Client")
     try:
         message = connectionSocket.recv(1024).decode()
@@ -55,7 +77,9 @@ while True:
         # Close client socket
         connectionSocket.close()
         print("")
+        animating = True
     except IOError:
+        stopAnimating()
         #Send response message for file not found
         response = 'HTTP/1.1 404 NOT FOUND\n'
         connectionSocket.send(response.encode())
@@ -64,4 +88,5 @@ while True:
         print("ERROR: File Not Found")
         print("User requested " + filename)
         print("")
+        animating = True
 graceful_exit()
